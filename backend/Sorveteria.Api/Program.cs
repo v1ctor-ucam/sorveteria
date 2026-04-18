4cef2a18a87399a3c6592d3bde6365796e8fcebe
v1ctor-ucam <joao.victor@candidomendes.edu.br>
2026-04-18 15:49:32 -0300

    fix: use EnsureCreatedAsync instead of MigrateAsync (no migrations exist)

--- backend/Sorveteria.Api/Program.cs
+++ backend/Sorveteria.Api/Program.cs
@@ -81,7 +81,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await db.Database.MigrateAsync();
+    await db.Database.EnsureCreatedAsync();
     await SeedData.EnsureSeedAsync(db);
 }
 

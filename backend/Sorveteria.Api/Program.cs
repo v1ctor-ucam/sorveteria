d9d5b00bd578631b97218a0911cf30f513e7a691
v1ctor-ucam <joao.victor@candidomendes.edu.br>
2026-04-18 15:46:42 -0300

    fix: add MigrateAsync before seed

--- backend/Sorveteria.Api/Program.cs
+++ backend/Sorveteria.Api/Program.cs
@@ -81,6 +81,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
     await SeedData.EnsureSeedAsync(db);
 }
 
